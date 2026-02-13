a54ca3b74edb0a65252f23b33bc190e8692326a7
Janchay <Janchay.la.68@ubu.ac.th>
2026-02-13 17:20:29 +0700

    Catty

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from oop682!")
-
+from Pyside6.QtWidgets import QApplication
+from sevices.file_log_viewer import FileLogViewer
+from sevices.mock_source import MockLogSource
+from ui.main_window import Mainwindow
 
 if __name__ == "__main__":
-    main()
+    app = QApplication([])
+    log = MockLogSource()
+    log = FileLogViewer("log/voters.log")
+    viewer = FileLogViewer()
+    viewer.show()
+    app.exec_()
